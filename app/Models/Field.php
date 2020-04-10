1e021c56637457d46550c8f9496bb7d182b08fdd
呼海斌 <136257981@qq.com>
2020-04-10 10:50:45 +0800

    增加关联模型类型

--- app/Models/Field.php
+++ app/Models/Field.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 class Field extends BaseModel
 {
+    // 单行文本
     const TYPE_SINGLE_TEXT = 'single_text';
+    // 多行文本
     const TYPE_MULTI_TEXT = 'multi_text';
+    // 富文本
     const TYPE_RICH_TEXT = 'rich_text';
+    // 附件
     const TYPE_ASSET = 'asset';
+    // 关联模型
+    const TYPE_REFERENCE = 'reference';
 
     public function project()
     {
