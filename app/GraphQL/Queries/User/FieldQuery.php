bca182b7a63be15da86b338629426fee4dbd54c3
BiZhenni <17611101084@163.com>
2021-03-12 15:01:55 +0800

    排序

--- app/GraphQL/Queries/User/FieldQuery.php
+++ app/GraphQL/Queries/User/FieldQuery.php
@@ -31,6 +31,11 @@ class FieldQuery extends BaseQuery
         ];
     }
 
+    protected function order()
+    {
+        return ['-id'];
+    }
+
     public function index($rootValue, array $args, GraphQLContext $context = null, ResolveInfo $resolveInfo)
     {
         $projectId = $context->request->this_project_id;
